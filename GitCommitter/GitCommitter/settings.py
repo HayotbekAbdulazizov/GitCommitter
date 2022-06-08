6b15023879c57a6e9e04e0d7ff56bd6c8f4dd2f2
hayotbekabdulazizov <hayotbekabdulazizov2005@gmail.com>
2022-06-09 04:36:16 +0500

    some msg

--- GitCommitter/GitCommitter/settings.py
+++ GitCommitter/GitCommitter/settings.py
@@ -123,8 +123,8 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-STATICFILES_DIRS = (
-  os.path.join(BASE_DIR, 'staticfiles')) ,
+# STATICFILES_DIRS = (
+#   os.path.join(BASE_DIR, 'staticfiles')) ,
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
